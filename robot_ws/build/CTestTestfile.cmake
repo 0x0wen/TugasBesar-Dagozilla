813655249f82ab0d30fb06bd03418e84fe429562
kindlygentleman <muhammadkamal52@gmail.com>
2023-05-31 15:56:15 +0700

    Cleaning Old Workspace

--- robot_ws/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rafly/Desktop/TUBES-Dagoz/robot_ws/src
-# Build directory: /home/rafly/Desktop/TUBES-Dagoz/robot_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
