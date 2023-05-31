4623bb2606d0b9180e7bf14d48d248cf68ee568a
Muhammad Kamal <96039140+KindlyGentleman@users.noreply.github.com>
2023-05-31 17:06:27 +0700

    Implementing v1.0 to Main Branch and Merging (#4)
    * Cleaning Old Workspace
    
    * Adding Message And Robot_msgs Package
    
    * Adding Control and Keyboard Node and Robot_control Package
    
    * Adding Launch File and Robot_launch Package
    
    * Adding Docummentation and  README.md File
    
    * Modifying The Control Node For Testing and Debugging

---
+++ jacobi-robot_ws/src/robot/robot_control/src/robot_keyboard_input_node.cpp
@@ -0,0 +1,37 @@
+#include "iostream"
+#include "ros/ros.h"
+#include "robot_msgs/KeyboardInput.h"
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "robot_keyboard_input_node");
+  ros::NodeHandle nh;
+  ros::Publisher pub = nh.advertise<robot_msgs::KeyboardInput>("/robot_keyboard_input",1000);
+
+  char input;
+  while (ros::ok()) {
+    std::cout << "Enter a command (W, A, S, D, R): ";
+    std::cin >> input;
+
+    // Check if the input is valid before publishing
+    if (input == 'W' || input == 'A' || input == 'S' || input == 'D' || input == 'R') {
+      robot_msgs::KeyboardInput msg;
+      msg.command = input;
+      pub.publish(msg);
+
+      // Log the command using rosinfo
+      ROS_INFO("Command: %c",input);
+    }
+    else {
+      std::cout << "Invalid command. Please try again.\n";
+    }
+
+    // Use ros::spinOnce() only when there are callbacks to process
+    ros::spinOnce();
+  }
+
+  // Log that the node is exiting using rosinfo
+  ROS_INFO("Robot keyboard input node is exiting.");
+
+  return 0;
+}
